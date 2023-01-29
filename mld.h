5bb8c21d5e1602c7e57dced2a9f0109e05a0a029
Harini Thiyagarajan <57031633+Harini-T@users.noreply.github.com>
2023-01-29 23:29:35 +0530

    Object database registration

--- mld.h
+++ mld.h
@@ -1,55 +1,92 @@
 #ifndef __MLD__
-
 #include <assert.h>
 
-//Structure Database definition begin
-#define MAX_STRUCTURE_NAME_SIZE	128
-#define MAX_FIELD_NAME_SIZE	128
+/*Structure Data base Definition Begin*/
 
-//Enumeration for data types
-typedef enum{
-	UINT8,
-	UINT32,
-	INT32,
-	CHAR,
-	OBJ_PTR,
-	FLOAT,
-	DOUBLE,
-	OBJ_STRUCT
-}data_type_t;
+#define MAX_STRUCTURE_NAME_SIZE 128
+#define MAX_FIELD_NAME_SIZE 128
+
+/*Enumeration for data types*/
+typedef enum {
+    UINT8,
+    UINT32,
+    INT32,
+    CHAR,
+    OBJ_PTR,
+    FLOAT,
+    DOUBLE,
+    OBJ_STRUCT
+} data_type_t;
+
+#define OFFSETOF(struct_name, fld_name)     \
+    (unsigned int)&(((struct_name *)0)->fld_name)
+
+#define FIELD_SIZE(struct_name, fld_name)   \
+    sizeof(((struct_name *)0)->fld_name)
 
 
-#define OFFSETOF(struct_name, fld_name)		(unsigned int)&(((struct_name*)0)->fld_name)
+typedef struct _struct_db_rec_t struct_db_rec_t;
 
-#define FIELD_SIZE(struct_name, fld_name)	sizeof(((struct_name*)0)->fld_name)
-
-typedef struct _struct_db_rec_t	struct_db_rec_t;
-
-//Structure to store the information of one field of a C structure
+/*Structure to store the information of one field of a 
+ * C structure*/
 typedef struct _field_info_{
-	char fname[MAX_FIELD_NAME_SIZE];	//Name of the field
-	data_type_t dtype;			//Datatype of the field
-	unsigned int size;			//size of field
-	unsigned int offset;			//Offset of field
-	//Below field is meaningful only if dtype = OBJ_PTR, or OBJ_STRUCT
-	char nested_str_name[MAX_STRUCTURE_NAME_SIZE];
-}field_info_t;
+    char fname [MAX_FIELD_NAME_SIZE];   /*Name of the field*/
+    data_type_t dtype;                  /*Data type of the field*/
+    unsigned int size;                  /*Size of the field*/
+    unsigned int offset;                /*Offset of the field*/
+    // Below field is meaningful only if dtype = OBJ_PTR, Or OBJ_STRUCT
+    char nested_str_name[MAX_STRUCTURE_NAME_SIZE];
+} field_info_t;
 
-//Structure to store the information of one C structure which could have 'n_fields' fields 
+/*Structure to store the information of one C structure
+ * which could have 'n_fields' fields*/
 struct _struct_db_rec_t{
-	struct_db_rec_t *next;				//Pointer to the next structure in the linked list
-	char struct_name[MAX_STRUCTURE_NAME_SIZE];	//key
-	unsigned int ds_size;				//Size of the structure
-	unsigned int n_fields;				//No of fields in the structure
-	field_info_t *fields;				//Pointer to the array of fields
+    struct_db_rec_t *next;  /*Pointer to the next structure in the linked list*/
+    char struct_name [MAX_STRUCTURE_NAME_SIZE];  // key
+    unsigned int ds_size;   /*Size of the structure*/
+    unsigned int n_fields;  /*No of fields in the structure*/
+    field_info_t *fields;   /*pointer to the array of fields*/
 };
 
-
-//The head of the linked list representing the structure database
+/*Finally the head of the linked list representing the structure
+ * database*/
 typedef struct _struct_db_{
-	struct_db_rec_t *head;
-	unsigned int count;
-}struct_db_t;
+    struct_db_rec_t *head;
+    unsigned int count;
+} struct_db_t;
+
+
+/*Structure Data base Definition Ends*/
+
+/* Printing functions*/
+void
+print_structure_rec (struct_db_rec_t *struct_rec);
+
+void
+print_structure_db(struct_db_t *struct_db);
+
+/* Fn to add the structure record in a structure database */
+
+int /*return 0 on success, -1 on failure for some reason*/
+add_structure_to_struct_db(struct_db_t *struct_db, struct_db_rec_t *struct_rec);
+
+/*Structure Registration helping APIs*/
+
+#define FIELD_INFO(struct_name, fld_name, dtype, nested_struct_name)    \
+   {#fld_name, dtype, FIELD_SIZE(struct_name, fld_name),                \
+        OFFSETOF(struct_name, fld_name), #nested_struct_name} 
+
+#define REG_STRUCT(struct_db, st_name, fields_arr)                    \
+    do{                                                               \
+        struct_db_rec_t *rec = calloc(1, sizeof(struct_db_rec_t));    \
+        strncpy(rec->struct_name, #st_name, MAX_STRUCTURE_NAME_SIZE); \
+        rec->ds_size = sizeof(st_name);                              \
+        rec->n_fields = sizeof(fields_arr)/sizeof(field_info_t);     \
+        rec->fields = fields_arr;                                    \
+        if(add_structure_to_struct_db(struct_db, rec)){              \
+            assert(0);                                               \
+        }                                                            \
+    }while(0);
 
 //Structure database definition ends
 
@@ -57,35 +94,42 @@ typedef struct _struct_db_{
 
 
 
-//Printing functions
-void print_structure_rec(struct_db_rec_t *struct_rec);
-
-void print_structure_db(struct_db_t *struct_db);
 
 
-//Function to add the structure record in a structure database
 
-int add_structure_to_struct_db(struct_db_t *struct_db, struct_db_rec_t *struct_rec);
-//Returns 0 on success, -1 on failure
+//Object database structure definitions starts here
+
+typedef struct _object_db_rec_ object_db_rec_t;
+
+struct _object_db_rec_{
+    object_db_rec_t *next;
+    void *ptr;
+    unsigned int units;
+    struct_db_rec_t *struct_rec;
+};
+
+typedef struct _object_db_{
+    struct_db_t *struct_db;
+    object_db_rec_t *head;
+    unsigned int count;
+} object_db_t;
 
 
-//Structure Registration helping APIs
+/*Dumping functions*/
+void
+print_object_rec(object_db_rec_t *obj_rec, int i);
 
-#define FIELD_INFO(struct_name, fld_name, dtype, nested_struct_name)	\
-	{#fld_name, dtype, FIELD_SIZE(struct_name,fld_name),		\
-		OFFSET(struct_name,fld_name), #nested_struct_name}
-		
+void
+print_object_db(object_db_t *object_db);
 
 
-#define REG_STRUCT(struct_db, st_name, fields_arr)				\
-	do{									\
-		struct_db_rec_t *rec = calloc(1,sieof(struct_db_rec_t));	\
-		strncpy(rec->struct_name, #st_name, MAX_STRUCTURE_NAME_SIZE);	\
-		rec->ds_size = sizeof(st_name);					\
-		rec->n_fields=sizeof(fields_arr)/sizeof(field_info_t);		\
-		rec->fields = fields_arr;					\
-		if(add_structure_to_struct_db(struct_db,rec)){			\
-			assert(0);						\
-		}								\
-	}while(0);
+
+/*API to malloc the object*/
+void*
+xcalloc(object_db_t *object_db, char *struct_name, int units);
+
+
+
+
+
 #endif /* __MLD__ */
